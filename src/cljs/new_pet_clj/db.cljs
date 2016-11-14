303e3583c8c00f0f8626178daefa65ce2d9811aa
Robert Gardner <rantingbob@gmail.com>
2016-11-14 11:10:51 -0800

    Updated Kit Selector

--- src/cljs/new_pet_clj/db.cljs
+++ src/cljs/new_pet_clj/db.cljs
@@ -2,11 +2,11 @@
 
 (def default-db
   {:selected-kit nil
-   :kits '({:name "Small Dog" :kit [{:name "Spooge" :code 123456}
-                                    {:name "Boobs" :code 456123}]}
+   :kits '({:name "Small Dog"  :kit [{:name "Spooge" :code 123456}
+                                     {:name "Boobs" :code 456123}]}
            {:name "Medium Dog" :kit [{:name "Big Ol Basket" :code 897}
                                      {:name "Bone" :code 123123}]}
-           {:name "Large Dog" :kit [{:name "Life-saving Whiskey" :code 1}
-                                    {:name "Cuddles" :code 2}]}
-           {:name "Cat" :kit [{:name "Evil" :code 394123455545}
-                              {:name "Cheese Weasel" :code 12334}]})})
+           {:name "Large Dog"  :kit [{:name "Life-saving Whiskey" :code 1}
+                                     {:name "Cuddles" :code 2}]}
+           {:name "Cat"        :kit [{:name "Evil" :code 394123455545}
+                                     {:name "Cheese Weasel" :code 12334}]})})
