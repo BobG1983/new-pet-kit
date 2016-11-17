bea49581fdfa552eb624c291e0d67ea3cde8f7bd
Robert Gardner <rantingbob@gmail.com>
2016-11-16 16:03:27 -0800

    removed unused requires

--- src/clj/new_pet_clj/api/routes.clj
+++ src/clj/new_pet_clj/api/routes.clj
@@ -1,10 +1,9 @@
 (ns new-pet-clj.api.routes
-  (:require [compojure.core :refer [GET POST defroutes context]]
-            [compojure.route :refer [not-found resources]]
-            [ring.util.response :refer [resource-response]]
-            [new-pet-clj.api.core :as api]))
+  (:require [compojure.core :refer [GET POST context]]))
 
 (def api-routes
   (context "/api" []
-    (GET "/" [] {:status 200 :body (str "GET Received")})
-    (POST "/" [] {:status 200 :body "POST Received"})))
+    (GET "/" [] {:status 200 :body "GET Received"})
+    (POST "/" [] {:status 200 :body "POST Received"})
+    (GET "/echo" request {:status 200 :body request})
+    (POST "/echo" request {:status 200 :body request})))
