625e45fbb4c6899f417fe48841f8df1717969c5b
Robert Gardner <rantingbob@gmail.com>
2016-11-23 13:28:19 -0800

    Timbre config

--- src/cljs/new_pet_clj/config.cljs
+++ src/cljs/new_pet_clj/config.cljs
@@ -1,4 +1,11 @@
-(ns new-pet-clj.config)
+(ns new-pet-clj.config
+  (:require [taoensso.timbre :as t]))
 
 (def DEBUG
   ^boolean js/goog.DEBUG)
+
+(defn config-logging
+  "Configured Timbre logging"
+  []
+  (let [level (if DEBUG :debug :info)]
+    (t/set-level! level)))
