d9de4a1c5cc6d3a4863646b66ae349e277dee9f4
Robert Gardner <rantingbob@gmail.com>
2016-11-24 03:01:04 -0800

    Rewritten handler again, this should now work in both dev and prod

--- src/clj/new_pet_clj/handler.clj
+++ src/clj/new_pet_clj/handler.clj
@@ -9,20 +9,24 @@
             [ring.middleware.transit :refer [wrap-transit-body wrap-transit-response]]
             [taoensso.timbre :as t]))
 
-;; Combine handlers
+;; Combine routes
+;; Enable proxy for Heroku and disable CSRF because we don't authenticate and users can't actually do
+;; anything damaging on the site and it's a pain.
 (def routes (wrap-defaults (cjr/routes api-routes site-routes)
-                           (if conf/DEBUG
-                             site-defaults
-                             (assoc secure-site-defaults :proxy true))))
+                           (t/spy (if conf/DEBUG
+                                      (-> site-defaults
+                                          (assoc-in [:security :anti-forgery] false))
+                                      (-> secure-site-defaults
+                                          (assoc :proxy true)
+                                          (assoc-in [:security :anti-forgery] false))))))
 
-
-;; Main handler
+;; Production handler
 (def prod-handler  (-> routes
                        (wrap-cors conf/CORS_POLICY)
                        (wrap-transit-body {:keywords? true :opts {}})
                        (wrap-transit-response)))
 
-;; Add dev mode wraps
+;; Development handler
 (def dev-handler (-> prod-handler
                      (wrap-reload)))
 
