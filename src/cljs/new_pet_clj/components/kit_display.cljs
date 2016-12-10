a5678b4777c339acac9b7735b69dde1281d3093e
Robert Gardner <rantingbob@gmail.com>
2016-12-09 16:21:38 -0800

    Updated amazon prime copy

--- src/cljs/new_pet_clj/components/kit_display.cljs
+++ src/cljs/new_pet_clj/components/kit_display.cljs
@@ -1,7 +1,7 @@
 (ns new-pet-clj.components.kit-display)
 
 (def cart-label "Kit Contents")
-(def amazon-prime-text "Need your kit quick?  Have an Amazon Prime 30-Day Free Trial on us.")
+(def amazon-prime-text "Need your kit quick?  Try Amazon Prime Free for 30-Days.")
 (def amazon-prime-url "https://www.amazon.com/tryprimefree?ref_=assoc_tag_ph_1427739975520&_encoding=UTF8&camp=1789&creative=9325&linkCode=pf4&tag=newpetkit-20&linkId=e607e2601f298cf3a974454106592444")
 
 (defn render-single-kit-element [elem]
