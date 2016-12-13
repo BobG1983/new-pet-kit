99b2e6bc4ab408f1586d1be9aed0ea7ca19788d6
Robert Gardner <rantingbob@gmail.com>
2016-12-12 16:55:51 -0800

    Added small dog kit

--- src/cljs/new_pet_clj/db.cljs
+++ src/cljs/new_pet_clj/db.cljs
@@ -5,9 +5,14 @@
    :cart nil
    :cart-status :inactive
    :selected-kit nil
-   ;; TODO: Make this stuff not nonsense
-   :kits         '({:name "Small Dog" :contents [{:name "Spooge" :code "B00QKSSGH0"}
-                                                 {:name "Boobs" :code "B0155XV9KG"}]}
+   :kits         '({:name "Small Dog" :contents [{:name "Blueberry Pet Collar" :code "B017UAIYW8"}
+                                                 {:name "Blueberry Pet Leash" :code "B01LWYVC7L"}
+                                                 {:name "American Kennel Club Bed" :code "B00FWQALD8"}
+                                                 {:name "Xentyx Pet Toys - 4 Pack" :code "B01E9TJMAQ"}
+                                                 {:name "Ranch Rewards Natural Rawhide Bone" :code "B000HHQ4UQ"}
+                                                 {:name "Bergan Heavy Duty Dog Bowl - 2 Pack" :code "B01FKLLGS8"}
+                                                 {:name "mDesign Silicon Feeding Mat" :code "B01EB6CRHW"}
+                                                 {:name "Pedigree Dry Dog Food - 1 Months Supply" :code "B016ZPB7QE"}]}
                    {:name "Medium Dog" :contents [{:name "Big Ol Basket" :code "B00QKSSGH0"}
                                                   {:name "Bone" :code "B0155XV9KG"}]}
                    {:name "Large Dog" :contents [{:name "Life-saving Whiskey" :code "B00QKSSGH0"}
